08ad017953211c73300685e6c85479223c3cc42a
mcoyne16 <77979084+mcoyne16@users.noreply.github.com>
2021-03-08 12:28:00 -0500

    Setup the repo

---
+++ app/robo_advisor.py
@@ -0,0 +1,34 @@
+# this is the "app/robo_advisor.py" file
+
+import requests
+import json
+
+request_url = "-----"
+
+response = requests.get(request_url)
+#print(type(response)) # class 'requests.models.response'
+#print(response.status__code) # 200
+#print(response.text)
+
+parsed_response = json.loads(response.text)
+
+breakpoint()
+
+quit()
+
+print("-------------------------")
+print("SELECTED SYMBOL: XYZ")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print("LATEST DAY: 2018-02-20")
+print("LATEST CLOSE: $100,000.00")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")
