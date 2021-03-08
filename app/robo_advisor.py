e79fc65774685d8af28e76de97d9a0059e3f5747
mcoyne16 <77979084+mcoyne16@users.noreply.github.com>
2021-03-08 16:12:48 -0500

    Get recent low price

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -19,14 +19,25 @@ last_refreshed = parsed_response['Meta Data']['3. Last Refreshed']
 
 tsd = parsed_response['Time Series (Daily)']
 
-dates = list(tsd.keys())
+dates = list(tsd.keys()) #TODO sort
 
 latest_day = dates[0]
 
 latest_close = tsd[latest_day]['4. close']
 
-#breakpoint()
+high_prices = []
+low_prices = []
 
+for date in dates:
+    high_price = tsd[date]["2. high"]
+    high_prices.append(float(high_price))
+    low_price = tsd[date]["3. low"]
+    low_prices.append(float(low_price))
+
+recent_high = max(high_prices) 
+recent_low = min(low_prices)
+
+#breakpoint()
 #quit()
 
 print("-------------------------")
@@ -37,8 +48,8 @@ print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print("LATEST DAY: ",last_refreshed)
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}" )
+print(f"RECENT LOW: {to_usd(float(recent_low))}" )
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
