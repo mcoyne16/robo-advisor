d4067e16ee476940c5bb8dca1f3b0298c90cdf27
mcoyne16 <77979084+mcoyne16@users.noreply.github.com>
2021-03-08 13:15:06 -0500

    Dynamically get latest day close

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -16,7 +16,14 @@ response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
 last_refreshed = parsed_response['Meta Data']['3. Last Refreshed']
-latest_close = parsed_response['Time Series (Daily)']['2021-03-05']['4. close']
+
+tsd = parsed_response['Time Series (Daily)']
+
+dates = list(tsd.keys())
+
+latest_day = dates[0]
+
+latest_close = tsd[latest_day]['4. close']
 
 #breakpoint()
 
