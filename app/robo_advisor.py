f80172b29d0e4ca9ecc92e156a780263be1f46a2
mcoyne16 <77979084+mcoyne16@users.noreply.github.com>
2021-03-08 13:07:47 -0500

    latest close in usd

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,19 +3,24 @@
 import requests
 import json
 
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
+
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
 
 response = requests.get(request_url)
-print(type(response)) # class 'requests.models.response'
-print(response.status_code)
-print(response.text)
+#print(type(response)) # class 'requests.models.response'
+#print(response.status_code)
+#print(response.text)
 
 parsed_response = json.loads(response.text)
 last_refreshed = parsed_response['Meta Data']['3. Last Refreshed']
+latest_close = parsed_response['Time Series (Daily)']['2021-03-05']['4. close']
 
 #breakpoint()
 
-quit()
+#quit()
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
@@ -23,8 +28,8 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: ", last_refreshed)
-print("LATEST CLOSE: $100,000.00")
+print("LATEST DAY: ",last_refreshed)
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
