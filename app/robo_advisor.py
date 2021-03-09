f320a2b63c6cab71d6153ca39211e4d8bd494e6c
mcoyne16 <77979084+mcoyne16@users.noreply.github.com>
2021-03-08 23:09:53 -0500

    Nested commands in while loop to analyze multiple stocks one at a time

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -29,109 +29,110 @@ while additional_stocks == True:
         tickers.append(ticker)
 
 for ticker in tickers:
-    #numeric_ticker if statement attributed to collaboration with Niko Restifo
+    #isnumeric reference: https://www.programiz.com/python-programming/methods/string/isnumeric
     numeric_ticker = False
     for i in range(len(ticker)):
         if ticker[i].isnumeric():
-            print(f"{ticker.upper()} is not valid. Expecting a properly-formed stock symbol like 'MSFT'. Please try again")
+            print(f"'{ticker.upper()}' is not valid. Expecting a properly-formed stock symbol like 'MSFT'. Please try again")
             break
     if numeric_ticker == True:
         continue
     if len(ticker) > 5:
-        print(f"{ticker.upper()} is not valid. Expecting a properly-formed stock symbol like 'MSFT'. Please try again")
+        print(f"'{ticker.upper()}' is not valid. Expecting a properly-formed stock symbol like 'MSFT'. Please try again")
         continue
-
-api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
+    api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
 #print(api_key)
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&apikey={api_key}"
-response = requests.get(request_url)
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&apikey={api_key}"
+    response = requests.get(request_url)
 #print(type(response)) # class 'requests.models.response'
 #print(response.status_code)
 #print(response.text)
-if "Error Message" in response.text:
-    print(f"Error, could not locate url.  Please try again.")
+    if "Error Message" in response.text:
+        print(f"Error, could not locate url.  Please try again.")
+        continue
+    if "higher API call frequency" in response.text:
+        print("You entered too many stocks.  Please try again.")
+        quit()
+    parsed_response = json.loads(response.text)
+    last_refreshed = parsed_response['Meta Data']['3. Last Refreshed']
+    tsd = parsed_response['Time Series (Daily)']
+    dates = list(tsd.keys()) #TODO sort
+    latest_day = dates[0]
+    latest_close = tsd[latest_day]['4. close']
 
-parsed_response = json.loads(response.text)
-last_refreshed = parsed_response['Meta Data']['3. Last Refreshed']
-tsd = parsed_response['Time Series (Daily)']
-dates = list(tsd.keys()) #TODO sort
-latest_day = dates[0]
-latest_close = tsd[latest_day]['4. close']
+    high_prices = []
+    low_prices = []
 
-high_prices = []
-low_prices = []
+    for date in dates:
+        high_price = tsd[date]["2. high"]
+        high_prices.append(float(high_price))
+        low_price = tsd[date]["3. low"]
+        low_prices.append(float(low_price))
 
-for date in dates:
-    high_price = tsd[date]["2. high"]
-    high_prices.append(float(high_price))
-    low_price = tsd[date]["3. low"]
-    low_prices.append(float(low_price))
+    recent_high = max(high_prices) 
+    recent_low = min(low_prices)
 
-recent_high = max(high_prices) 
-recent_low = min(low_prices)
-
-if float(latest_close) <= float(recent_low):
-    recommendation = "BUY"
-    reason = "The stock's latest closing is less than its recent low"
-else:
-    recommendation = "SELL"
-    reason = "The stock's latest closing is greater than its recent low"
+#recommendation
+    if float(latest_close) <= float(recent_low)* 1.2:
+        recommendation = "BUY"
+        reason = "The stock's latest closing is less than 20% above its recent low"
+    else:
+        recommendation = "SELL"
+        reason = "The stock's latest closing is greater than 20% above its recent low"
 
 #OUTPUTS
 #
 
-csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", f"prices_{ticker}.csv")
+    csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", f"prices_{ticker}.csv")
     #csv_file_path = "data/prices.csv" # a relative filepath
 
-csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
-with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
-    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
-    writer.writeheader() # uses fieldnames set above
-    for date in dates:
-        daily_prices = tsd[date]
-        writer.writerow({
-            "timestamp": date, 
-            "open": daily_prices["1. open"],
-            "high": daily_prices["2. high"],
-            "low": daily_prices["3. low"],
-            "close": daily_prices["4. close"],
-            "volume": daily_prices["5. volume"],
-        })
+    csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+    with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+        writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+        writer.writeheader() # uses fieldnames set above
+        for date in dates:
+            daily_prices = tsd[date]
+            writer.writerow({
+                "timestamp": date, 
+                "open": daily_prices["1. open"],
+                "high": daily_prices["2. high"],
+                "low": daily_prices["3. low"],
+                "close": daily_prices["4. close"],
+                "volume": daily_prices["5. volume"],
+            })
 
-print("-------------------------")
-print("SELECTED SYMBOL:", ticker.upper())
-print("-------------------------")
-print("REQUESTING STOCK MARKET DATA...")
-date = datetime.date.today()
-time = datetime.datetime.now()
-print("REQUEST AT:", date, time.strftime("%I:%M:%S %p"))
-print("-------------------------")
-print("LATEST DAY: ",last_refreshed)
-print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print(f"RECENT HIGH: {to_usd(float(recent_high))}" )
-print(f"RECENT LOW: {to_usd(float(recent_low))}" )
-print("-------------------------")
-print(f"RECOMMENDATION: {recommendation}")
-print(f"RECOMMENDATION REASON: {reason}")
-print("-------------------------")
-print(f"WRITING DATA TO CSV {csv_file_path}...")
+    print("-------------------------")
+    print("SELECTED SYMBOL:", ticker.upper())
+    print("-------------------------")
+    print("REQUESTING STOCK MARKET DATA...")
+    date = datetime.date.today()
+    time = datetime.datetime.now()
+    print("REQUEST AT:", date, time.strftime("%I:%M:%S %p"))
+    print("-------------------------")
+    print("LATEST DAY: ",last_refreshed)
+    print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
+    print(f"RECENT HIGH: {to_usd(float(recent_high))}" )
+    print(f"RECENT LOW: {to_usd(float(recent_low))}" )
+    print("-------------------------")
+    print(f"RECOMMENDATION: {recommendation}")
+    print(f"RECOMMENDATION REASON: {reason}")
+    print("-------------------------")
+    print(f"WRITING DATA TO CSV {csv_file_path}...")
 
 #line graph of csv data for past 100 days
 
-csv_filename = os.path.join(os.path.dirname(__file__), "..", "data", f"prices_{ticker}.csv")
-line_graph = input("Would you like to view a line graph plotting the prices of the stock over time? [y]/n : ")
-if line_graph == "y":
-    prices_df = read_csv(csv_filename)
-    #seaborn and matplotlib combo attributed to Prof Rossetti slack comment from December 3, 2020
-    sns.lineplot(data=prices_df, x="timestamp", y="close")
-    #same attribution^
-    plt.title(f"Price of {ticker.upper()} stock over the past 100 days")
-    plt.xticks(rotation = 90, fontsize=3)
-    plt.show()
-else:
-    print("-------------------------")
-    print("HAPPY INVESTING!")
-    print("-------------------------")
+    csv_filename = os.path.join(os.path.dirname(__file__), "..", "data", f"prices_{ticker}.csv")
+    line_graph = input("Would you like to view a line graph plotting the prices of the stock over time? [y]/n : ")
+    if line_graph == "y":
+        prices_df = read_csv(csv_filename)
+        #seaborn and matplotlib combo attributed to Prof Rossetti slack comment from December 3, 2020
+        sns.lineplot(data=prices_df, x="timestamp", y="close")
+        #same attribution^
+        plt.title(f"Price of {ticker.upper()} stock over the past 100 days")
+        plt.xticks(rotation = 90, fontsize=3)
+        plt.show()
+    else:
+        print("-------------------------")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
